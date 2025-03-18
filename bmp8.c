572f5ca1aa9b1f19bf6f70a91a6cd4a3ff3ad080
Noxapeuh <cyrrousselle59@gmail.com>
2025-03-18 09:59:15 +0100

    Fonctions printInfo et loadImage

--- bmp8.c
+++ bmp8.c
@@ -3,6 +3,31 @@
 #include <stdlib.h>
 
 t_bmp8 * bmp8_loadImage(const char * filename){
-    FILE * file = fopen(filename, "r");
+    FILE * file = fopen(filename, "rb");
+    t_bmp8 *img;
+    fread(img->header, 1, 54, file);
+    img->width = *(unsigned int *)(img->header + 18);
+    img->height = *(unsigned int *)(img->header + 22);
+    img->colorDepth = *(unsigned int *)(img->header + 28);
+    img->data = *(unsigned int *)(img->header + 34);
+    fclose(file);
+    return img;
+}
 
+void bmp8_printInfo(t_bmp8 * img) {
+    printf("Image Info :\n");
+    printf("\tWidth : %d\n", img->width);
+    printf("\tHeight : %d\n", img->height);
+    printf("\tColor Depth : %d\n", img->colorDepth);
+    printf("\tSize : %d\n", img->height*img->width);
+}
+
+
+int main(){
+    t_bmp8 * bmp8;
+    bmp8 = bmp8_loadImage( "lena_color.bmp" );
+    bmp8_printInfo( bmp8 );
+
+
+  return 0;
 }
