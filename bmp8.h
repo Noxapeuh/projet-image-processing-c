98c8f4425edb593d35889b3626cfba154d715fc2
Noxapeuh <cyrrousselle59@gmail.com>
2025-04-27 15:30:19 +0200

    Finished first part

--- bmp8.h
+++ bmp8.h
@@ -69,6 +69,7 @@ typedef struct {
 
 t_pixel ** bmp24_allocateDataPixels (int width, int height);
 void bmp24_freeDataPixels (t_pixel ** pixels, int height);
+
 t_bmp24 * bmp24_allocate (int width, int height, int colorDepth);
 void bmp24_free (t_bmp24 * img);
 
